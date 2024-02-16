4abd792b786933be5c0ea07b78a385116d8853e6
av-egorova <154748705+av-egorova@users.noreply.github.com>
2024-02-16 13:56:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,85 +1,53 @@
-﻿/*void PrintRange(int n, int m = 1)
-{
-if (m > n)
-{
-Console.WriteLine();
-return;
-}
+// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
-Console.Write($"{m} ");
-
-PrintRange(n, m + 1);
-}
-
-PrintRange(5);
-
-PrintRange(12);*/
-
-
-
-/*Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 void PrintNumbers(int m, int n)
 {
-if (m <= n)
-{
-Console.Write(m + " "); 
-PrintNumbers(m + 1, n); 
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        PrintNumbers(m + 1, n);
+    }
 }
-}
 
 
 {
-Console.Write("Write number m: ");
-int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Write number m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Write number n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Write number n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-PrintNumbers(m, n);
-Console.WriteLine();
-}*/
+    PrintNumbers(m, n);
+    Console.WriteLine();
+}
 
-/*// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// 2 задача. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 int akkerman(int m, int n)
 {
-if (m == 0) 
-return n + 1;
-else 
-if (n == 0) 
-return akkerman(m - 1, 1);
-else 
-return akkerman(m - 1, akkerman(m, n - 1));
+    if (m == 0)
+        return n + 1;
+    else
+    if (n == 0)
+        return akkerman(m - 1, 1);
+    else
+        return akkerman(m - 1, akkerman(m, n - 1));
 }
 
 
 {
-Console.Write("Write number n: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Write number m: ");
-int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Write number n: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Write number m: ");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"Function Akkerman =  {akkerman(n, m)} ");
-}*/
+    Console.Write($"Function Akkerman =  {akkerman(n, m)} ");
+}
 
 
 
 // 3 задача. Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
-
-
-// int[] arr = { 1, 2, 3, 4, 5 };
-//         PrintArrayReverse(arr, arr.Length - 1);
-
-
-//      void PrintArrayReverse(int[] arr, int index)
-//     {
-//         if (index < 0)
-//             return;
-
-//         Console.Write(arr[index]);
-//         PrintArrayReverse(arr, index - 1);
-//     }
 
 
 int[] array = { 1, 2, 5, 0, 10, 34 };
